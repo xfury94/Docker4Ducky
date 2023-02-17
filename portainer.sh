73809e6f112fabe9704220a3e015fed39b68d5b1
lesta <lesta@ALPHA>
2023-02-17 22:37:43 +0000

    Add project files.

---
+++ portainer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade
+
+apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+
+apt-cache policy docker-ce
+
+apt install docker-ce
+
+
+docker volume create portainer_data && docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
